c0c0d19d5b2777f319bd39dafcdda58cc8379273
Stephan <stephan.pabinger@gmail.com>
2018-11-12 12:21:52 +0100

    Update of sam_examiner

--- day1/sam_examiner.py
+++ day1/sam_examiner.py
@@ -22,82 +22,76 @@ class SamExaminer:
     
 
 
-    def __init__(self):
+    def __init__(self, bam_file_path, file_name):
         ## Your gene of interest
         self.gene = "PRMT3" # You can choose any gene you are interested in, as long as it is on chr11
-		
-
-    
-    def fetch_gene_coordinates(self, genome_reference, file_name):
         
-        print "Connecting to UCSC to fetch data"
+        print("Using the BAM file: " + str(bam_file_path))
+        self.bam_file_path = bam_file_path
+                
+        print("Fetching gene coordinates")
         
-        ## Open connection
-        cnx = mysql.connector.connect(host='genome-mysql.cse.ucsc.edu', user='genomep', passwd='password', db=genome_reference)
-        
-        ## Get cursor
-        cursor = cnx.cursor()
-        
-        ## Build query fields
-        query_fields = ["refGene.name2",
-                        "refGene.name",
-                        "refGene.chrom",
-                        "refGene.txStart",
-                        "refGene.txEnd",
-                        "refGene.strand",
-                        "refGene.exonCount",
-                        "refGene.exonStarts",
-                        "refGene.exonEnds"]
-        
-        ## Build query
-        query = "SELECT DISTINCT %s from refGene" % ",".join(query_fields)
-        
-        ## Execute query
-        cursor.execute(query)
-        
-        ## Write to file
-        ## TODO this may need some work 
-        with open(file_name, "w") as fh:
-            for row in cursor:
-                fh.write(str(row) + "\n")
-    
+        ## Check if the gene coordinates file exists
+        if os.path.exists(file_name):
+            with open(file_name) as fh_in:
+                for row in csv.reader(fh_in, delimiter="\t"):
+                    if row[0] == self.gene:
+                        print("found row")
+                        info = row
+                        
+                        self.gene = info[0]
+                        self.name = info[1]
+                        self.chrom = info[2]
+                        self.txStart = int(info[3])
+                        self.txEnd = int(info[4])
+                        self.strand = info[5]
+                        self.exonCount = info[6]
+                        self.exonStarts = info[7] # Das ist ein bytestring
+                        self.exonEnds = info[8] # Das ist ein bytestring
+                    
+            print("Done loading fetched data")
             
-        ## Close cursor & connection
-        cursor.close()
-        cnx.close()
         
-        print "Done fetching data"
+        print("Done fetching data")
+                
                 
     def get_sam_header(self):
-        print "todo"
+        print("todo")
+        
         
     def get_properly_paired_reads_of_gene(self):
-        print "todo"
+        print("todo")
         
+                
     def get_gene_reads_with_indels(self):
-        print "todo"
+        print("todo")
+        
         
     def get_number_mapped_reads(self):
-        print "todo"
+        print("todo")
+                
         
     def get_gene_symbol(self):
-        print "todo"
+        print("todo")
+        
         
     def get_region_of_gene(self):
-        print "ads"
+        print("ads")
+        
         
     def get_number_of_exons(self):
-        print "ads"
+        print("ads")
+        
     
     def print_summary(self):
-        print "Print all results here"
+        print("Print all results here")
     
         
 if __name__ == '__main__':
-    print "Practical 1"
-    sam_examiner = SamExaminer()
+    print("SamExaminer")
+    sam_examiner = SamExaminer("", "gene_coordinates.txt") ## Change the bam file here
     
-    sam_examiner.fetch_gene_coordinates("hg19", "MYFILE.TXT") ## TODO change filename
+    
     sam_examiner.print_summary()
     
     
